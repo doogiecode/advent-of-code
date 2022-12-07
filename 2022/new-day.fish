329e767ea90f7b146cc899bcc5ebf0402402f351
William Proffitt <proffitt@elderresearch.com>
2022-12-07 17:45:23 -0500

    New day script

---
+++ 2022/new-day.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+
+if test "$argv[1]" = ""
+  echo 'Please provide a directory name:'
+  read DIR_NAME
+else
+  set DIR_NAME $argv[1]
+end
+
+mkdir -p $DIR_NAME/src
+touch $DIR_NAME/src/m.clj
+touch $DIR_NAME/deps.edn
+echo "{:deps {}}" >> $DIR_NAME/deps.edn
